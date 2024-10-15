c83c74622b741e7124c57c10f1170f8524e252e8
Marcelo Ribeiro Xavier da Silva <marcelorxs@gmail.com>
2024-10-15 12:18:42 +0100

    Changes jira_script to allow more than on queue

--- jira_script.rb
+++ jira_script.rb
@@ -10,25 +10,34 @@ require_relative 'jira_client'
 # chmod a+x PATH/jira_script.rb
 
 # This makes the colour pick between: green (#02A61E), orange (#FC8900) or red (#FF0000)
-# green is for zero, orange is for between 1 and 5 and red for 6 and on
+# green is for zero, orange is for between 1 and 3 and red for 4 and on
 def pick_hex_colour(tickets_size)
   case tickets_size
   when 0
     '#02A61E'
-  when 1..5
+  when 1..3
     '#FC8900'
   else
     '#FF0000'
   end
 end
 
-queue = JiraClient.new.get_queue(servicedesk_id: 1, queue_id: 18)
+def queues(ids)
+  queues_ids = ids.is_a?(Array) ? ids : [ids]
+  ids.map { |id| JiraClient.new.get_queue(servicedesk_id: 1, queue_id: id) }
+end
 
-puts "#{queue['size']} :ticket: | size=16 color=#{pick_hex_colour(queue['size'])}"
+# Scheduler and Workforce respectively
+all_queues = queues([18, 20])
+size = all_queues.sum { |queue| queue['size'].to_i }
+
+tickets = all_queues.reduce([]) { |values, queue| values + queue['values'] }
+
+puts "#{size} :ticket: | size=16 color=#{pick_hex_colour(size)}"
 puts '---'
-puts 'No :ticket: to solve :tada:' if queue['size'].zero?
+puts 'No :ticket: to solve :tada:' if size.zero?
 
-queue['values'].each do |ticket|
+tickets.each do |ticket|
   url = "https://jobandtalent.atlassian.net/browse/#{ticket['key']}"
   if ticket.dig('fields', 'status', 'name').end_with?('support')
     puts "#{ticket['key']} - #{ticket.dig('fields', 'summary')} | href=#{url} size=12"
